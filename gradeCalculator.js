bca554e7411613b7c0c536aaaff43931ead6e1ec
Anirbandasjoy <joy600508@gmail.com>
2025-07-08 23:10:19 +0600

    add code

---
+++ gradeCalculator.js
@@ -0,0 +1,17 @@
+let mark = 32;
+
+if (mark >= 80) {
+  console.log("Your grade is A+");
+} else if (mark >= 70) {
+  console.log("Your grade is A");
+} else if (mark >= 60) {
+  console.log("Your grade is A-");
+} else if (mark >= 50) {
+  console.log("Your grade is B");
+} else if (mark >= 40) {
+  console.log("Your grade is C");
+} else if (mark >= 33) {
+  console.log("Your grade is D");
+} else {
+  console.log("You are fail");
+}
---
+++ passOrFail.js
@@ -0,0 +1,7 @@
+let mark = 33;
+
+if (mark >= 33) {
+  console.log("you are pass");
+} else {
+  console.log("you are fail");
+}
